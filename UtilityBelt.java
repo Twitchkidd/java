760a4974aee6cf51486b008ca8dd1bacc07f0beb
Gareth Field <field.gareth@gmail.com>
2021-04-17 06:11:15 -0400

    first commit

---
+++ UtilityBelt.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class UtilityBelt {
+  public static void main(String[] args) {
+    sout('bruh');
+  }
+
+  public static void sout(String text) {
+    System.out.println(text);
+  }
+}
